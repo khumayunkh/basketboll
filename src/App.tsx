2fb5f785e2ed20d9044f03a241420d227eace49f
khumayunkh <hasanovhumayun15@gmail.com>
2022-10-25 20:41:28 +0500

    information

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/header';
+import Information from './components/Information/Information';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Information/>
     </div>
   );
 }
---
+++ src/components/Information/Information.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import './information.sass'
+import klub from './../img/klub.png'
+import vijesti from './../img/vijesti.png'
+import momcad from './../img/momcad.png'
+
+function Information(){
+    return(
+        <>
+        <div className="information-container">
+            <div className="information-info">
+                <div className="information-info-item1">
+                    
+                </div>
+                <div className="information-info-item2">
+                    
+                </div>
+                <div className="information-info-item3">
+                    
+                </div>
+                <div className="information-info-item4">
+                    
+                </div>
+                <div className="information-info-item5">
+                    
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Information

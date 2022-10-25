677ebff300ddcc68847adc57d1e13bf4a05b547d
khumayunkh <hasanovhumayun15@gmail.com>
2022-10-25 05:17:16 +0500

    dropdown menu

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import  './header.sass'
 import background from './../img/background.png'
+import {Nav} from 'react-bootstrap';
+import {Navbar} from 'react-bootstrap';
+import {NavDropdown} from 'react-bootstrap';
 import logo from './../img/logo.png'
 
 function Header(){
@@ -16,7 +19,23 @@ function Header(){
                     </div>
                     <h2 className="header_menu__navbar__name">KLUB</h2>
                     <h2 className="header_menu__navbar__name">VIJESTI</h2>
-                    <h2 className="header_menu__navbar__name">MOMČAD</h2>
+                    <Nav>
+                        <NavDropdown
+                            id="nav-dropdown-dark-example"
+                            title="Dropdown"
+                            menuVariant="dark"
+                        >
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">
+                            Another action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
                     <h2 className="header_menu__navbar__name">KONTAKT</h2>
                 </div>
                 <div className="header_menu_info">

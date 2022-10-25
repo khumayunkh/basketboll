63f7b28cde13cb97d1f71ea776fa95e3051581bd
khumayunkh <hasanovhumayun15@gmail.com>
2022-10-25 05:23:09 +0500

    style

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -22,18 +22,13 @@ function Header(){
                     <Nav>
                         <NavDropdown
                             id="nav-dropdown-dark-example"
-                            title="Dropdown"
+                            title="MOMČAD"
                             menuVariant="dark"
                         >
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">
-                            Another action
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.1">igrači</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">stožer</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">utakmice</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.4">tablica</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                     <h2 className="header_menu__navbar__name">KONTAKT</h2>

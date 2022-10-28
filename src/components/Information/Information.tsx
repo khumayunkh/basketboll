cac6cafb6546f761a9e49088993b6376e11884b7
khumayunkh <hasanovhumayun15@gmail.com>
2022-10-29 01:04:54 +0500

    stadium

--- src/components/Information/Information.tsx
+++ src/components/Information/Information.tsx
@@ -12,38 +12,40 @@ function Information(){
             <div className="information-info">
                 <div className="information-info-item1">
                     <img src={klub}/>
+                    <div>
                     <div className="information-info_title">
                         <div className="information-info_title_triangle-left"></div>
                         <h2 className="information-info_title_name">KLUB</h2>
                     </div>
-                    <h3>FUNCUTI ISPUNILI DVORANU</h3>
+                    <h3>FUNCUTI ISPUNILI DVORANU</h3></div>
                 </div>
                 <div className="information-info-item2">
                     <img src={vijesti}/>
-                    <div className="information-info_title">
+                   <div className="information-info_mobile_version"><div className="information-info_title">
                         <div className="information-info_title_triangle-left"></div>
                         <h2 className="information-info_title_name2">VIJESTI</h2>
                     </div>
                     <h4>Funcuti pozivaju na Šubičevac!</h4>
+                    </div> 
                 </div>
                 <div className="information-info-item3">
-                    <img src={stadium}/>
+                    <img className="information-info-stadium" src={stadium}/>
                 </div>
                 <div className="information-info-item4">
                     <img src={momcad}/>
-                    <div className="information-info_title">
+                    <div className="information-info_mobile_version"><div className="information-info_title">
                         <div className="information-info_title_triangle-left"></div>
                         <h2 className="information-info_title_name2">MOMČAD</h2>
                     </div>
-                    <h4>Trener Strunje o novim igračima</h4>
+                    <h4>Trener Strunje o novim igračima</h4></div>
                 </div>
                 <div className="information-info-item5">
                     <img src={momcad}/>
-                    <div className="information-info_title">
+                    <div className="information-info_mobile_version"><div className="information-info_title">
                         <div className="information-info_title_triangle-left"></div>
                         <h2 className="information-info_title_name2">MOMČAD</h2>
                     </div>
-                    <h4>Trener Strunje o novim igračima</h4>
+                    <h4>Trener Strunje o novim igračima</h4></div>
                 </div>
             </div>
         </div>
